bd5b208c39e220f280f900f3a47f8b76e4ad116d
Sohail <67971304+sohail-op@users.noreply.github.com>
2022-12-08 00:25:06 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -2,11 +2,8 @@ package codex;
 /* 
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 below 1000.
+Find the sum of all the  multiples of 3 or 5 below 1000.
 */
-import java.util.*;
-
-import javax.crypto.spec.IvParameterSpec;
 public class q1 
 {
     public static void main(String[] args) 
